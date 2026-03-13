315780aa6b0a32d6836335690476de5514f3dbfb
Equipo <Equipo@LAPTOP-EUUN4F1D>
2026-03-13 16:11:33 +0100

    Post Refactorización

--- Deposito/src/cuentas/CCuenta.java
+++ Deposito/src/cuentas/CCuenta.java
@@ -1,7 +1,40 @@
+package cuentas;
 
 public class CCuenta {
 	private String nombre;
-    private String cuenta;
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInterés() {
+		return tipoInterés;
+	}
+
+	public void setTipoInterés(double tipoInterés) {
+		this.tipoInterés = tipoInterés;
+	}
+
+	private String cuenta;
     private double saldo;
     private double tipoInterés;
 
